7ddc337d14a479e801edaf07f577dc863e394d1d
typicallhavok <diamondpineappleboy@gmail.com>
2023-11-19 01:29:36 +0530

    Delete x.py

--- x.py
+++
@@ -1,5 +0,0 @@
-import mysql.connector as sql
-sqlc = sql.connect(host="localhost", user="root", password="Amogh2004", database="zomapes")
-cur=sqlc.cursor()
-cur.execute("insert into log1 values('1','1');")
-sqlc.commit()
