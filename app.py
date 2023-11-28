06318ed7ec25578b0c6395ef4bb9524421c90c0b
typicallhavok <diamondpineappleboy@gmail.com>
2023-11-28 19:30:29 +0530

    80% done

---
+++ app.py
@@ -0,0 +1,123 @@
+from flask import Flask, request, render_template, redirect, url_for
+from twilio.rest import Client
+import random
+import mysql.connector as sql
+
+app = Flask(__name__)
+sqlc = sql.connect(
+    host = 'localhost',
+    user = 'root',
+    password = 'Amogh2004',
+    database = 'zomapes'
+)
+cur=sqlc.cursor(buffered=True)
+account_sid = 'ACe93a1e1c711c40d0b8ab76679e07fd2e'
+auth_token = '4842a1873fab465da57b302f2e8dacb6'
+client = Client(account_sid, auth_token)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        session={}
+        srn = request.form.get('srn')
+        cur.execute(f'select phone from log1 where SRN="{srn}";')
+        ph=cur.fetchone()
+        if ph:
+            gotp = genotp(ph)
+            session[ph]=gotp
+            return redirect(url_for('home',srn=srn, session=session))
+    return render_template('index.html')
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    srn = request.args.get('srn')
+    session = request.args.get('session')
+    if session is None:
+        return redirect(url_for('index'))
+    if request.method == 'POST':
+        entered_otp = request.form.get('otp')
+        session = request.args.get('session')
+        cur.execute(f'select wallet from log1 where SRN="{srn}";')
+        wallet=cur.fetchone()[0]
+        if entered_otp==session[21:27]:
+            cur.execute("select Food,Price from zomamenu;")
+            data=[]
+            x=cur.fetchall()
+            for i in x:
+                di1={}
+                di1['food']=i[0]
+                di1['price']=i[1]
+                data.append(di1)
+            cur.execute(f"select * from orders where srn='{srn}';")
+            x=cur.fetchall()
+            orders=[]
+            for i in x:
+                di1={}
+                di1['orderno']=i[0]
+                di1['order']=i[1]
+                di1['price']=i[2]
+                di1['transactionid']=i[3]
+                di1['status']=i[4]
+                di1['quantity']=i[5]
+                orders.append(di1)
+            cur.execute(f"select * from carts where srn='{srn}';")
+            x=cur.fetchall()
+            print(x)
+            cart=[]
+            for i in x:
+                di1={}
+                di1['food']=i[0]
+                di1['price']=i[1]
+                di1['quantity']=i[2]
+                cart.append(di1)
+            return render_template('home.html', data=data, orders=orders, wallet=wallet, cart=cart, srn=srn, success=True)
+    return render_template('home.html', success=False)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        cod=request.form.get('pcode')
+        srn=request.form.get('srn')
+        phone=request.form.get('phone')
+        phno=cod+phone
+        if srn and phone:
+            cur.execute(f"insert into log1 values('{srn}','{phno}')")
+            sqlc.commit()
+            return 'registered'
+    return render_template('register.html',data=[{'code':'+91'}, {'code':'+619'}, {'code':'+69'}])
+
+@app.route('/cartAdd', methods=['POST'])
+def cartAdd():
+    quant = request.get_json()
+    if quant[2]>1:
+        cur.execute(f'update carts set quantity={quant[2]} where srn="{quant[3]}" and food="{quant[0]}";')
+    else:
+        cur.execute(f'insert into carts values{tuple(quant)};')
+    sqlc.commit()
+    return 'added'
+
+@app.route('/cartSub', methods=['POST'])
+def cartSub():
+    quant = request.get_json()
+    if quant[2]>0:
+        cur.execute(f'update carts set quantity={quant[2]} where srn="{quant[3]}" and food="{quant[0]}";')
+    elif quant[2]==0:
+        cur.execute(f'delete from carts where srn="{quant[3]}" and food="{quant[0]}";')
+    sqlc.commit()
+    return 'removed'
+
+def genotp(ph):
+    gotp = random.randint(100000, 999999)
+    # message = client.messages.create(
+    #     from_='+12164382762',
+    #     body=f'Your zomapes otp is {gotp}',
+    #     to=ph
+    # )
+    print(gotp)
+    return gotp
+
+def validate_otp(eotp, genotp):
+    return eotp == genotp
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
--- static/js/home.js
+++ static/js/home.js
@@ -1,22 +1,124 @@
 const indicator = document.querySelector('.nav-indicator');
 const items = document.querySelectorAll('.nav-item');
+const menu = document.querySelector('#Menu');
+const wall = document.querySelector('#Wallet');
+const support = document.querySelector('#Support');
+const orders = document.querySelector('#Orders');
+const srn = document.querySelector('.srn').innerHTML;
 
-function handleIndicator(el) {
-  items.forEach(item => {
-    item.classList.remove('is-active');
-    item.removeAttribute('style');
-  });
-  
-  indicator.style.width = `${el.offsetWidth}px`;
-  indicator.style.left = `${el.offsetLeft}px`;
-  indicator.style.backgroundColor = el.getAttribute('active-color');
-
-  el.classList.add('is-active');
-  el.style.color = el.getAttribute('active-color');
+function closeAll() {
+    menu.style.display = 'none';
+    wall.style.display = 'none';
+    support.style.display = 'none';
+    orders.style.display = 'none';
 }
 
+function openMenu() {
+    menu.style.display = 'grid';
+}
 
-items.forEach((item, index) => {
-  item.addEventListener('click', (e) => { handleIndicator(e.target)});
-  item.classList.contains('is-active') && handleIndicator(item);
+function handleIndicator(el) {
+    items.forEach(item => {
+        item.classList.remove('is-active');
+        item.removeAttribute('style');
+    });
+    
+    indicator.style.width = `${el.offsetWidth}px`;
+    indicator.style.left = `${el.offsetLeft}px`;
+    indicator.style.backgroundColor = el.getAttribute('active-color');
+
+    el.classList.add('is-active');
+    el.style.color = el.getAttribute('active-color');
+}
+
+items.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+        handleIndicator(e.target);
+    });
+    item.classList.contains('is-active') && handleIndicator(item);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            closeAll();
+            const targetId = this.getAttribute('href');
+            const targetSection = document.querySelector(targetId);
+            if (targetId=='#Menu') {
+                targetSection.style.display = 'grid';
+            }
+            else {
+                targetSection.style.display = 'flex';
+            }
+        });
+    });
+    const countButtons = document.querySelectorAll('.addd');
+    const subtractButtons = document.querySelectorAll('.sub');
+    const countDisplay = document.querySelectorAll('.countc');
+    const food = document.querySelectorAll('.food');
+    const price = document.querySelectorAll('.price');
+    const checkoutDisplay = document.querySelector('.chkt');
+
+    countButtons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            const count = Number(countDisplay[index].innerHTML);
+            countDisplay[index].innerHTML = String(count + 1);
+            const selectedFood = food[index].innerHTML;
+            const foodPrice = Number(price[index].innerHTML.replace('\u20b9',''));
+            var sqlAdd = [selectedFood, foodPrice, count+1, srn];
+            fetch('/cartAdd', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(sqlAdd)
+            })
+            .then(response => response.text())
+            .catch(error => console.error('Error:', error));
+            cdisplay(countDisplay,checkoutDisplay);
+        });
+    });
+
+    subtractButtons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            const count = Number(countDisplay[index].innerHTML);
+            const selectedFood = food[index].innerHTML;
+            const foodPrice = Number(price[index].innerHTML.replace('\u20b9',''));
+            var sqlSub = [selectedFood, foodPrice, count-1, srn];
+            if (count > 0) {
+                countDisplay[index].innerHTML = String(count - 1);
+            }
+            fetch('/cartSub', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(sqlSub)
+            })
+            .then(response => response.text())
+            .catch(error => console.error('Error:', error));
+            cdisplay(countDisplay,checkoutDisplay);
+        });
+    });
+});
+
+function cdisplay(y,checkoutDisplay) {
+    let chkitems='';
+    let nitems=y.length;
+    s='';
+    for(let i=0;i<nitems;i++) {
+        s+='0';
+    }
+    y.forEach((p,index) => {
+        chkitems+=y[index].innerHTML;
+    });
+    if (chkitems!=s) {
+        checkoutDisplay.style.display='flex';
+    }
+    else {
+        checkoutDisplay.style.display='none';
+    }
+}
+
